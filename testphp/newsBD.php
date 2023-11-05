050c1624790a71e79f0a5a2b2b3bc09bae49bc9b
Korneevec Ilia <korneevecin@mail.ru>
2023-11-05 06:49:05 +0300

    fix

--- testphp/newsBD.php
+++ testphp/newsBD.php
@@ -1,8 +1,8 @@
 <?php
-$mysql_host = "localhost"; // sql сервер
-$mysql_user = "a0872478_StudgorodokDB"; // пользователь
-$mysql_password = "BkmzRjhyttdtw2003!"; // пароль
-$mysql_database = "a0872478_StudgorodokDB"; // имя базы данных chat
+$mysql_host = "localhost"; 
+$mysql_user = "a0872478_StudgorodokDB"; 
+$mysql_password = "BkmzRjhyttdtw2003!"; 
+$mysql_database = "a0872478_StudgorodokDB"; 
 $charset = 'utf8';
 $connection = new mysqli($mysql_host, $mysql_user, $mysql_password, $mysql_database);
 if ($connection->connect_error) {
