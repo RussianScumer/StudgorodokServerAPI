eeeaa824adc8a52c1ec406910ac855a9f194c526
C0rTeZ <120840631+C0rTeZ13@users.noreply.github.com>
2023-11-19 13:13:15 +0300

    Добавил adminsDB.php

---
+++ testphp/adminsDB.php
@@ -0,0 +1,32 @@
+<?php
+$mysql_host = "localhost"; 
+$mysql_user = "a0872478_StudgorodokDB"; 
+$mysql_password = "BkmzRjhyttdtw2003!"; 
+$mysql_database = "a0872478_StudgorodokDB"; 
+$charset = 'utf8';
+$connection = new mysqli($mysql_host, $mysql_user, $mysql_password, $mysql_database);
+if ($connection->connect_error) {
+    die("ConnectError".$connection->connect_error);
+}
+if(!$connection->set_charset($charset)){
+    echo "EncodeError";
+}
+
+// Обработка GET запроса
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $result = $connection->query("SELECT admins FROM adminsDB");
+    $rows = array();
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $row["admins"] = "http://a0872478.xsph.ru/" . $row["admins"];
+            $rows[] = $row;
+        }
+        header('Content-Type: application/json');
+        echo json_encode($rows);
+    } else {
+        echo "0 results";
+    }
+}
+$connection->close();
+?>
